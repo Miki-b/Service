a85a5abe97e295ec6b0f18d91976d908856bee75
mikib01 <michaelbehailu0@email.com>
2025-04-15 16:49:47 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: application/json');
+$path = $_GET['route'] ?? '';
+
+switch ($path) {
+    case 'hello':
+        echo json_encode(["message" => "Hello there!"]);
+        break;
+    case 'user':
+        echo json_encode(["user" => "Seen Dev"]);
+        break;
+    default:
+        http_response_code(404);
+        echo json_encode(["error" => "Not Found"]);
+}
